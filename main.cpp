c5e537c0c07d9c4e11012f43fa3784cd5c0e4286
YarikVor <62393229+YarikVor@users.noreply.github.com>
2020-05-23 00:34:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include "windows.h"
+#include <cstdlib>
+#include <iostream>
+
 using namespace std;
-unsigned int microseconds;
-int main(){
-	Sleep(1000);
-	return 0;
+
+int main()
+{
+ FILE *f; int dat;
+ srand(time(0));
+ int n=rand()%30 + 1;
+ cout << "File name? ";
+ char s[20];
+ cin.getline(s, 20);
+ f=fopen(s, "wb");
+ for (int i=1; i<=n; i++)
+ { dat = rand()%101 - 50;
+   cout << dat << " ";
+   fwrite(&dat, sizeof(int), 1, f);
+ }
+ cout << endl;
+ fclose(f);
+    system("PAUSE");
+    return EXIT_SUCCESS;
 }
